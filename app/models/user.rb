219a66751de23d16f498ffc65c0e92fcfe1ad39b
Hyakon <azn.baoul@gmail.com>
2020-12-02 10:47:51 +0100

    welcome email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :welcome_send
+
   has_many :orders
   has_one :artist
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
