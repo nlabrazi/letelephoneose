675a58dac91398920c19df224389800a2adef832
Hyakon <azn.baoul@gmail.com>
2020-12-04 13:14:56 +0100

    Add form for new order and according modification

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,30 +1,25 @@
 class OrdersController < ApplicationController
 
-  def index
-    @user = current_user
-    @artist = Artist.find(params[:artist_id])
-    @availabilities = @artist.availabilities
-    authorize @user
-  end
-
   def show
     @order = Order.find(params[:id])
   end
 
   def new
+    @order = Order.new
     @user = current_user
     @artist = Artist.find(params[:artist_id])
-    @availabilities = @artist.availabilities
-    authorize @user
-
-  end
-  def create
-    @user = current_user
     @availability = Availability.find(params[:availability_id])
-    @order = Order.new
+    authorize @user
+  end
+
+  def create
+    @order = Order.new(order_params)
     @order.user = current_user
+    @availability = Availability.find(order_availability_params)
+    @order.availability = @availability
+
     @order.service_id = Service.all.sample.id
-    @order.availability_id = @availability.id
+
     @availability.is_booked = true
     if @order.save && @availability.save
       flash.notice = "Votre réservation a bien été créée"
@@ -33,14 +28,17 @@ class OrdersController < ApplicationController
       flash.alert = "Une erreur est survenue #{@order.errors.messages}"
       redirect_to root_path
     end
-    authorize @user
     authorize @order
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:user_id,:availibility_id)
+    params.require(:order).permit(:target,:description)
+  end
+
+  def order_availability_params
+    params.require(:availability_id)
   end
 
 end
