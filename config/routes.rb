0d958c1ac3c9abd6805c0aa1e417fe39009be983
rtdeedqgryet <borismariondorier@gmail.com>
2020-12-08 14:48:27 +0100

    add stripe v3 (fonctionnal)

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
   resources :dashboard, only: [:index]
 
   resources :charges
-
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+  end
 end
 
