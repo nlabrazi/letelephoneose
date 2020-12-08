34789f075134083b8ea239f8619746836f8dcdb7
bkany <bkany@student.42.fr>
2020-12-08 19:54:47 +0100

    adding the contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,12 @@
-class User < ApplicationRecord
+class Contact < ApplicationRecord
+
+	validates :name, :presence => :true
+	validates :email, 
+    :presence => :true,
+    :format => { 
+      :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+      :message => "L'adresse mail doit être valide."
+    }
+  validates :message, :presence => :true
 
 end
