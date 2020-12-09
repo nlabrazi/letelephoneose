46f3047e7872882906ae9b88db069a48332ca656
bkany <bkany@student.42.fr>
2020-12-09 12:11:41 +0100

    contactpage changes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,12 @@
 class Contact < ApplicationRecord
 
-	validates :name, :presence => :true
+	validates :name, :presence => :true, :error => "Votre nom est obligatoire."
 	validates :email, 
     :presence => :true,
     :format => { 
       :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
       :message => "L'adresse mail doit être valide."
+      :error => 'Votre email est obligatoire.'
     }
   validates :message, :presence => :true
 
