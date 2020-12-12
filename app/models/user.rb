50795329cca33940e8274135352ba189210f2b37
SimonMfroy <mainfroy.simon@gmail.com>
2020-12-12 11:02:15 +0100

    a small adjust for validates and charges new

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,6 @@ class User < ApplicationRecord
 
   after_create :welcome_send
 
-  validates :first_name, length: { in: 1..50 }
-  validates :last_name, length: { in: 1..50 }
-  validates :phone, numericality: { only_integer: true },
-            length: { is: 10 }
   self.per_page = 5
 
   include PgSearch
